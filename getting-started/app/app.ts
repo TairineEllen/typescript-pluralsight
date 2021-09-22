43e54dd6681fe78d26ed10976000cbdbff382642
TairineEllen <tairine.ellen@gmail.com>
2021-09-22 19:24:20 -0300

    declare my first variable with type annotation

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,6 +1,13 @@
 function startGame() {
+  const playerName: string = 'Alice';
+  logPlayer(playerName);
+
   let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'; /*! = nonnull assertion operator*/
+}
+
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
