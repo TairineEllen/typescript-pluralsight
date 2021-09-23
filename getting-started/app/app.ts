321797e110f6dbec55492eadf0925c5ff3b6e010
TairineEllen <tairine.ellen@gmail.com>
2021-09-22 21:53:10 -0300

    add functions getInputValue and postScore with type annotations and default parameters values

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,13 +1,26 @@
 function startGame() {
-  let playerName: string = 'Tairine';  
+  let playerName: string | undefined = getInputValue('playername');  
   logPlayer(playerName);
-
-  let messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'; /*! = nonnull assertion operator*/
+  postScore(8); 
 }
 
-function logPlayer(name: string): void {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score:number, playerName: string = 'MultiMath Player'): void {
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`; /*! = nonnull assertion operator*/
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
