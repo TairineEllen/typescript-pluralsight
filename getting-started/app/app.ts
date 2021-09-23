231d30ae5d1422ddf477f16f65c92fa49e6fcb89
TairineEllen <tairine.ellen@gmail.com>
2021-09-22 22:20:40 -0300

    use function types in postScore function

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,7 +1,7 @@
 function startGame() {
   let playerName: string | undefined = getInputValue('playername');  
   logPlayer(playerName);
-  postScore(8); 
+  postScore(8, playerName); 
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -19,8 +19,21 @@ function getInputValue(elementID: string): string | undefined {
 }
 
 function postScore(score:number, playerName: string = 'MultiMath Player'): void {
+  let logger: (value: string) => void;
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
   const scoreElement: HTMLElement | null = document.getElementById('postedScores');
   scoreElement!.innerText = `${score} - ${playerName}`; /*! = nonnull assertion operator*/
+  logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+  console.error(err);
+}
