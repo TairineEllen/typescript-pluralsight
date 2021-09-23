1aaa43f54d42d3f81e7e140dc6f5a97e8107b9fb
TairineEllen <tairine.ellen@gmail.com>
2021-09-22 21:22:16 -0300

    add type annotations to parameters and return values

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,12 +1,12 @@
 function startGame() {
-  const playerName: string = 'Alice';
+  let playerName: string = 'Tairine';  
   logPlayer(playerName);
 
   let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'; /*! = nonnull assertion operator*/
 }
 
-function logPlayer(name) {
+function logPlayer(name: string): void {
   console.log(`New game starting for player: ${name}`);
 }
 
